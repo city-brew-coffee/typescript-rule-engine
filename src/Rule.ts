eda0fac9a7d456fdf75f445f19274dff5390e5f2
Marcus Twichel <marc.twichy@gmail.com>
2020-07-08 16:15:39 -0600

    Added support for rules to return generic type

--- src/Rule.ts
+++ src/Rule.ts
@@ -1,8 +1,8 @@
 import { parseJson } from './JsonObject';
 
-export abstract class Rule<T> {
+export abstract class Rule<T, R = boolean> {
   abstract type: string;
-  abstract isValid(input: T): boolean;
+  abstract isValid(input: T): R;
   toJson = () => JSON.stringify(this);
   toObject = () => parseJson(JSON.stringify(this));
 }
